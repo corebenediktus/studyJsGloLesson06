f40f02fe670e429d3887060f012fc6a24a0429e3
corebenediktus <ivico@inbox.ru>
2021-02-08 01:05:00 +0300

    ready game on lesson06

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 'use strict'
 
-
 const game = function() {
-    let secretNumber = 50;
-    let userNumber = prompt('Загадай случайное число от 1 до 100');
-
-    const tryUserNumber = function() {
-        let isNumber = function(userNumber) {
-            return !isNaN(parseFloat(userNumber)) && isFinite(userNumber)
-        };
-
-        if (userNumber !== null) {
-            let correctNumber=isNumber;
-            if (correctNumber===secretNumber) {
-                alert('Ты угадал число! Сыграем снова?');
-                game();
-            } else if (correctNumber<50) {
-                alert('Не угадал, твое число меньше, попробуй еще!!');
-                game();
-            } else if (correctNumber>50) {
-                alert('Не угадал, твое число больше, попробуй еще!!');
-                game();
-            }
-        
+    const userNumber = prompt('Угадай случайное число от 1 до 100');
+    
+    let isNumber = function(userNumber) {
+        return !isNaN(parseFloat(userNumber)) && isFinite(userNumber);
+    };
+    
+    if (isNumber(userNumber)==true) {
+        const tryUserNumber = function() {
+            let secretNumber = 50;
+            
+            if (userNumber!==null) {
+                if (userNumber==secretNumber) {
+                    alert('Ты угадал! попробуем снова?');
+                    game();
+                } else if (userNumber<50) {
+                    alert('Загаданное число больше');
+                    game();
+                } else if (userNumber>50) {
+                    alert('Загаданное число меньше');
+                    game();
+                }   
+            } 
+        }
+        tryUserNumber();
+    } else if  (userNumber==null) {
+        alert('Игра окончена! Сыграем снова?');
+        game();
+    } else {
+        alert('Введи число по условию');
+        game();
     }
-    tryUserNumber();
-
-
-
-}       
+}
 game();
-
-
-// let isNumber = function(n) {
-//     return !isNaN(parseFloat(n)) && isFinite(n)
-// };
-
-// проверить на соответствие числу
-// проверить на соответствие нулю
-// проверить на свопадение с загаданным числом
-//     если отмена то закрыть
-//     если угадал то победа + еще раз?
